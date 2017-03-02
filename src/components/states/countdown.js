2b70b6075b09bec9136fddfc48315fe3d7af6988
Fernando Serrano <fernandojsg@gmail.com>
2017-03-02 13:57:35 +0100

    Fixes #52 added first countdown

--- src/components/states/countdown.js
+++ src/components/states/countdown.js
@@ -5,16 +5,26 @@ AFRAME.registerComponent('countdown', {
     this.interval = window.setInterval(this.countdown, 1000);
     this.floor = document.getElementById('floor');
     this.floor.setAttribute('discofloor', {pattern: '' + this.countdownTime});
-    var textElement = this.textElement = document.getElementById('centeredText');
-    textElement.setAttribute('visible', true);
-    textElement.setAttribute('text', {value: this.countdownTime.toString()});
+    var textElements = this.textElements = document.querySelectorAll('#centeredText, #counter0Text, #counter1Text');
+
+    for (var i = 0; i < textElements.length; i++) {
+      var textElement = textElements[i];
+      textElement.setAttribute('visible', true);
+      textElement.setAttribute('text', {value: 'ready'});
+    }
     this.opacity = 1;
   },
 
   countdown: function () {
     this.countdownTime--;
     this.opacity = 1;
-    this.textElement.setAttribute('text', {value: this.countdownTime.toString()});
+
+    for (var i = 0; i < this.textElements.length; i++) {
+      var textElement = this.textElements[i];
+      textElement.setAttribute('visible', true);
+      textElement.setAttribute('text', {value: this.countdownTime === 2 ? 'set' : 'Dance!'});
+    }
+
     this.el.setAttribute('game-state', 'countdownTime', this.countdownTime);
     this.floor.setAttribute('discofloor', {pattern: '' + this.countdownTime});
     if (this.countdownTime === 0) { window.clearInterval(this.interval); }
@@ -22,10 +32,14 @@ AFRAME.registerComponent('countdown', {
 
   tick: function (time, delta) {
     this.opacity -= delta*0.001;
-    this.textElement.setAttribute('text', {opacity: this.opacity});
+    for (var i = 0; i < this.textElements.length; i++) {
+      this.textElements[i].setAttribute('text', {opacity: this.opacity});
+    }
   },
 
   remove: function () {
-    this.textElement.setAttribute('visible', false);
+    for (var i = 0; i < this.textElements.length; i++) {
+      this.textElements[i].setAttribute('visible', false);
+    }
   }
 });
