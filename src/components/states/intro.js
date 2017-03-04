414c6c869700ef0c7ef8265e94d9bac50f127c79
dmarcos <diego.marcos@gmail.com>
2017-03-03 21:24:53 -0800

    Loading default dance the same as a remote one

--- src/components/states/intro.js
+++ src/components/states/intro.js
@@ -1,53 +1,59 @@
 var getUrlParams = require('../../utils').getUrlParams;
 var loadJSONFromUrl = require('../../utils').loadJSONFromUrl;
+var defaultDanceData = require('json!../../../assets/dance.json');
 
 AFRAME.registerComponent('intro', {
   init: function () {
     var urlParams = getUrlParams();
+    var self = this;
     if (urlParams.url) {
-      var self = this;
-      this.el.sceneEl.systems['uploadcare'].download(urlParams.url, function (data) {
-        var selectedAvatarEl = document.getElementById(data.content.avatar);
-        var selectedAvatarHeadEl = selectedAvatarEl.querySelector('.head');
-        var selectedAvatarRightHandEl = selectedAvatarEl.querySelector('.rightHand');
-        var selectedAvatarLeftHandEl = selectedAvatarEl.querySelector('.leftHand');
-        var avatarHeadEl = document.getElementById('avatarHead');
-        var rightHandEl = document.getElementById('rightHand');
-        var leftHandEl = document.getElementById('leftHand');
-        var cameraRig = document.getElementById('cameraRig');
-        self.el.sceneEl.setAttribute('game-state', 'selectedAvatar', selectedAvatarEl);
-        avatarHeadEl.setAttribute('gltf-model', selectedAvatarHeadEl.getAttribute('gltf-model'));
-        avatarHeadEl.setAttribute('visible', false);
-        rightHandEl.setAttribute('visible', false);
-        leftHandEl.setAttribute('visible', false);
-
-
-        cameraRig.setAttribute('rotation', '0 180 0');
-        rightHandEl.setAttribute('gltf-model', selectedAvatarRightHandEl.getAttribute('gltf-model'));
-        leftHandEl.setAttribute('gltf-model', selectedAvatarLeftHandEl.getAttribute('gltf-model'));
-
-        self.el.setAttribute('avatar-replayer', {
-          spectatorMode: true,
-          loop: true
-        });
-
-        document.getElementById('spectatorCamera').setAttribute('position', '0 1.6 2');
-        window.setTimeout(function () {
-          avatarHeadEl.setAttribute('visible', true);
-          rightHandEl.setAttribute('visible', true);
-          leftHandEl.setAttribute('visible', true);
-          self.el.components['avatar-replayer'].startReplaying(data.content.recording);
-          self.el.emit('dancing');
-          selectedAvatarEl.querySelector('[sound]').components.sound.playSound();
-        }, 5000);
+      this.el.sceneEl.systems['uploadcare'].download(urlParams.url, function(data) {
+        self.loadDance(data.content);
       });
     } else {
-      this.el.setAttribute('avatar-replayer', {
-        src: 'assets/dance.json',
-        spectatorMode: true,
-        loop: true
-      });
+      this.loadDance(defaultDanceData);
     }
+  },
+
+  loadDance: function (data) {
+    var self = this;
+    var selectedAvatarEl = document.getElementById(data.avatar);
+    var selectedAvatarHeadEl = selectedAvatarEl.querySelector('.head');
+    var selectedAvatarRightHandEl = selectedAvatarEl.querySelector('.rightHand');
+    var selectedAvatarLeftHandEl = selectedAvatarEl.querySelector('.leftHand');
+    var avatarHeadEl = document.getElementById('avatarHead');
+    var rightHandEl = document.getElementById('rightHand');
+    var leftHandEl = document.getElementById('leftHand');
+    var cameraRig = document.getElementById('cameraRig');
+    if (!this.el.hasLoaded) {
+      this.el.addEventListener('loaded', function (){
+        self.loadDance(data);
+      });
+      return;
+    }
+    this.el.sceneEl.setAttribute('game-state', 'selectedAvatar', selectedAvatarEl);
+    avatarHeadEl.setAttribute('gltf-model', selectedAvatarHeadEl.getAttribute('gltf-model'));
+    avatarHeadEl.setAttribute('visible', false);
+    rightHandEl.setAttribute('visible', false);
+    leftHandEl.setAttribute('visible', false);
+    cameraRig.setAttribute('rotation', '0 180 0');
+    rightHandEl.setAttribute('gltf-model', selectedAvatarRightHandEl.getAttribute('gltf-model'));
+    leftHandEl.setAttribute('gltf-model', selectedAvatarLeftHandEl.getAttribute('gltf-model'));
+
+    this.el.setAttribute('avatar-replayer', {
+      spectatorMode: true,
+      loop: true
+    });
+
+    document.getElementById('spectatorCamera').setAttribute('position', '0 1.6 2');
+    window.setTimeout(function () {
+      avatarHeadEl.setAttribute('visible', true);
+      rightHandEl.setAttribute('visible', true);
+      leftHandEl.setAttribute('visible', true);
+      self.el.components['avatar-replayer'].startReplaying(data.recording);
+      self.el.emit('dancing');
+      selectedAvatarEl.querySelector('[sound]').components.sound.playSound();
+    }, 5000);
   },
 
   remove: function () {
